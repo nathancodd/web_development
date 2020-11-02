a290cf967efa9cf0a6be23997ec8de719dea2c1f
Nathan Codd <nathanmcodd@gmail.com>
2020-11-01 23:18:53 -0600

    npm scripts

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+            $("#mycarousel").carousel( {interval: 2000} );
+            $("#carouselButton").on("click", function() {
+                if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+                    $("#mycarousel").carousel("pause");
+                    $("#carouselButton").children("span").removeClass("fa-pause");
+                    $("#carouselButton").children("span").addClass("fa-play");
+                }
+                else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+                    $("#mycarousel").carousel("cycle");
+                    $("#carouselButton").children("span").removeClass("fa-play");
+                    $("#carouselButton").children("span").addClass("fa-pause");
+                }
+            });
+        });
+        
+        $("a#reserveTable").on("click", function() {
+            $("#reserveModal").modal("toggle");
+        });
+        $("a#loginButton").on("click", function() {
+            $("#loginModal").modal("toggle");
+        });
